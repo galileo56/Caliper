bb14b90a931ea91a4eff2a9253c4090508293342
Vicent <vmateu@usal.es>
2017-05-12 14:07:06 +0200

    refinements

--- lib/NRQCD.F90
+++ lib/NRQCD.F90
@@ -475,9 +475,9 @@ module NRQCDClass
 
     rho = 3 * mc/2/mb/alpha; rho2 = rho**2; rho4 = rho2**2; rho3 = rho * rho2
 
-    DeltaCharm = 4 * mb * alpha**3/pi/27 * (  11._dp/3 - 3 * pi * rho/2 + &
-    4 * rho2 - 2 * pi * rho3 - 2 * (2 - rho2 - 4 * rho4)/sqrt(rho2 - 1) * &
-    Atan( (rho - 1)/(rho + 1) )  )
+    DeltaCharm = 4 * mb * alpha**3/pi/27 * (   11._dp/3 - 3 * pi * rho/2 + &
+    4 * rho2 - 2 * pi * rho3 - 2 * (2 - rho2 - 4 * rho4)/sqrt(rho2 - 1)  * &
+    Atan(  sqrt( (rho - 1)/(rho + 1) )  )   )
 
   end function DeltaCharm
 
