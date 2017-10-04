c3dd82b7528852099d5352aa4e5512038d149a9e
Vicent Mateu <galileo56@gmail.com>
2017-10-04 20:02:21 +0200

    bla

---
+++ build/CMakeFiles/subdir_mods.dir/cmake_clean_Fortran.cmake
@@ -0,0 +1,118 @@
+# Remove fortran modules provided by this target.
+FILE(REMOVE
+  "adapt.mod"
+  "ADAPT.mod"
+  "CMakeFiles/subdir_mods.dir/adapt.mod.stamp"
+
+  "alphaclass.mod"
+  "ALPHACLASS.mod"
+  "CMakeFiles/subdir_mods.dir/alphaclass.mod.stamp"
+
+  "anomdimclass.mod"
+  "ANOMDIMCLASS.mod"
+  "CMakeFiles/subdir_mods.dir/anomdimclass.mod.stamp"
+
+  "carlson_elliptic_module.mod"
+  "CARLSON_ELLIPTIC_MODULE.mod"
+  "CMakeFiles/subdir_mods.dir/carlson_elliptic_module.mod.stamp"
+
+  "chaplin.mod"
+  "CHAPLIN.mod"
+  "CMakeFiles/subdir_mods.dir/chaplin.mod.stamp"
+
+  "constants.mod"
+  "CONSTANTS.mod"
+  "CMakeFiles/subdir_mods.dir/constants.mod.stamp"
+
+  "cumulantclass.mod"
+  "CUMULANTCLASS.mod"
+  "CMakeFiles/subdir_mods.dir/cumulantclass.mod.stamp"
+
+  "derigamma.mod"
+  "DERIGAMMA.mod"
+  "CMakeFiles/subdir_mods.dir/derigamma.mod.stamp"
+
+  "electroweakclass.mod"
+  "ELECTROWEAKCLASS.mod"
+  "CMakeFiles/subdir_mods.dir/electroweakclass.mod.stamp"
+
+  "gapmassclass.mod"
+  "GAPMASSCLASS.mod"
+  "CMakeFiles/subdir_mods.dir/gapmassclass.mod.stamp"
+
+  "hyper.mod"
+  "HYPER.mod"
+  "CMakeFiles/subdir_mods.dir/hyper.mod.stamp"
+
+  "hypergeo.mod"
+  "HYPERGEO.mod"
+  "CMakeFiles/subdir_mods.dir/hypergeo.mod.stamp"
+
+  "kernelsclass.mod"
+  "KERNELSCLASS.mod"
+  "CMakeFiles/subdir_mods.dir/kernelsclass.mod.stamp"
+
+  "legendre.mod"
+  "LEGENDRE.mod"
+  "CMakeFiles/subdir_mods.dir/legendre.mod.stamp"
+
+  "massivensclass.mod"
+  "MASSIVENSCLASS.mod"
+  "CMakeFiles/subdir_mods.dir/massivensclass.mod.stamp"
+
+  "matrixelementsclass.mod"
+  "MATRIXELEMENTSCLASS.mod"
+  "CMakeFiles/subdir_mods.dir/matrixelementsclass.mod.stamp"
+
+  "mctopclass.mod"
+  "MCTOPCLASS.mod"
+  "CMakeFiles/subdir_mods.dir/mctopclass.mod.stamp"
+
+  "modelclass.mod"
+  "MODELCLASS.mod"
+  "CMakeFiles/subdir_mods.dir/modelclass.mod.stamp"
+
+  "nonsingularclass.mod"
+  "NONSINGULARCLASS.mod"
+  "CMakeFiles/subdir_mods.dir/nonsingularclass.mod.stamp"
+
+  "nrqcdclass.mod"
+  "NRQCDCLASS.mod"
+  "CMakeFiles/subdir_mods.dir/nrqcdclass.mod.stamp"
+
+  "poly.mod"
+  "POLY.mod"
+  "CMakeFiles/subdir_mods.dir/poly.mod.stamp"
+
+  "profilesclass.mod"
+  "PROFILESCLASS.mod"
+  "CMakeFiles/subdir_mods.dir/profilesclass.mod.stamp"
+
+  "quadpack.mod"
+  "QUADPACK.mod"
+  "CMakeFiles/subdir_mods.dir/quadpack.mod.stamp"
+
+  "rnrqcdclass.mod"
+  "RNRQCDCLASS.mod"
+  "CMakeFiles/subdir_mods.dir/rnrqcdclass.mod.stamp"
+
+  "runningclass.mod"
+  "RUNNINGCLASS.mod"
+  "CMakeFiles/subdir_mods.dir/runningclass.mod.stamp"
+
+  "sigmaclass.mod"
+  "SIGMACLASS.mod"
+  "CMakeFiles/subdir_mods.dir/sigmaclass.mod.stamp"
+
+  "singularclass.mod"
+  "SINGULARCLASS.mod"
+  "CMakeFiles/subdir_mods.dir/singularclass.mod.stamp"
+
+  "toppikclass.mod"
+  "TOPPIKCLASS.mod"
+  "CMakeFiles/subdir_mods.dir/toppikclass.mod.stamp"
+
+  "vfnsmsrclass.mod"
+  "VFNSMSRCLASS.mod"
+  "CMakeFiles/subdir_mods.dir/vfnsmsrclass.mod.stamp"
+  )
